6c6f2874ace78611a30332794605c1cd00fc036a
sakamototaisei <taisei0403089@gmail.com>
2021-05-17 14:01:11 +0000

    added sign_up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+
+  def show
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_16_134252) do
+ActiveRecord::Schema.define(version: 2021_05_17_010651) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
